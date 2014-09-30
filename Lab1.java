8fa24928fe9c8d8300a5a3bae0a51ddea5be4643
brehmpj09 <brehmpj09@uww.edu>
2014-09-30 10:29:37 -0500

    first commit

---
+++ Lab1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Lab1 {
+	public static void main(String[] args){
+		
+		double num = Math.random()*9+1;
+		int realnum = (int)num;
+		int guess;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a number between 1 and 10: ");
+		guess = scan.nextInt();
+		if (realnum ==guess){
+			System.out.print("You were right!");
+		}else{
+			System.out.print("You were wrong, the number was "+realnum);
+		}
+	}
+
+}
