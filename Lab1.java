c079c709c1a7f1145a0ef4d03e1229e815ba7002
Your Name <brehmpj09@uww.edu>
2015-02-05 16:31:59 -0600

    modified:   Lab1.java.v1.2

--- Lab1.java
+++ Lab1.java
@@ -4,15 +4,18 @@ public class Lab1 {
 		
 		double num = Math.random()*9+1;
 		int realnum = (int)num;
-		int guess;
+		int guess, numOfGuesses=0;
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter a number between 1 and 10: ");
 		for (;;){
 		guess = scan.nextInt();
+		numOfGuesses+=1;
 		if (realnum ==guess){
 			System.out.print("You were right!");
+			System.out.println(numOfGuesses);
 		}else{
 			System.out.print("You were wrong, the number was "+realnum);
+			System.out.println(numOfGuesses);
 		}
 	}
 }
